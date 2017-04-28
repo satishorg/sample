0a8ce6a61d18b26e7f46275bb37efe68d83bb29c
[sruthish512] <[Satish.Alavala0904@gmail.com]>
2017-04-28 22:57:30 +0530

    Added comments in the script

--- check_ip.py
+++ check_ip.py
@@ -2,8 +2,11 @@ import iptools
 from iptools import IpRangeList, netmask2prefix, validate_cidr
 import os
 
+#Add allowed IP ranges
 r = IpRangeList('202.202.0.0/16')
 
+
+#function to validate the ip address
 def validate_sg_ip():
     print (r)
     iip = raw_input('Enter your IP: ')
